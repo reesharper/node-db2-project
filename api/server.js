31a9aa623de6de3630a18dd6c40904f9aab4ded6
reesharper <reestimharper@gmail.com>
2021-01-19 18:10:23 -0500

    project is complete!

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-const fruitsRouter = require('./cars/cars-router');
+const carsRouter = require('./cars/cars-router');
 
 const server = express();
 
 server.use(express.json());
 
-server.use('/api/cars', fruitsRouter);
+server.use('/api/cars', carsRouter);
 
 module.exports = server;
